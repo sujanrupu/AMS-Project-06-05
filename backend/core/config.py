abe22d8bc5081a3a3d505f83b98bf5db4c2c34f8
Madhurima-Ghosh18 <ghoshmadhurima2002@gmail.com>
2026-05-06 10:33:43 +0530

    new changes

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,19 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    JIRA_EMAIL = os.getenv("JIRA_EMAIL")
+    JIRA_EMAIL     = os.getenv("JIRA_EMAIL")
     JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
-    JIRA_DOMAIN = os.getenv("JIRA_DOMAIN")
+    JIRA_DOMAIN    = os.getenv("JIRA_DOMAIN")
     SERVICE_DESK_ID = os.getenv("SERVICE_DESK_ID")
     REQUEST_TYPE_ID = os.getenv("REQUEST_TYPE_ID")
 
     SUPABASE_URL = os.getenv("SUPABASE_URL")
     SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-<<<<<<< HEAD
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-=======
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
     SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
->>>>>>> d0486dcc903ac38b1d8a01d690a0881436a94549
